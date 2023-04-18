3626aefb7f7e46dcd51bcdc5296a6877e09ee49f
MoRa-sdu <MoRa-sdu>
2023-04-18 22:24:45 +0800

    对ui包部分文件进行注释

--- app/src/main/java/net/micode/notes/ui/NoteItemData.java
+++ app/src/main/java/net/micode/notes/ui/NoteItemData.java
@@ -1,33 +1,18 @@
-/*
- * Copyright (c) 2010-2011, The MiCode Open Source Community (www.micode.net)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package net.micode.notes.ui;
-
+ 
 import android.content.Context;
 import android.database.Cursor;
 import android.text.TextUtils;
-
+ 
 import net.micode.notes.data.Contact;
 import net.micode.notes.data.Notes;
 import net.micode.notes.data.Notes.NoteColumns;
 import net.micode.notes.tool.DataUtils;
-
-
+ 
+ 
 public class NoteItemData {
     static final String [] PROJECTION = new String [] {
+        //常量标记和数据意义即其翻译字面义
         NoteColumns.ID,
         NoteColumns.ALERTED_DATE,
         NoteColumns.BG_COLOR_ID,
@@ -41,7 +26,7 @@ public class NoteItemData {
         NoteColumns.WIDGET_ID,
         NoteColumns.WIDGET_TYPE,
     };
-
+    //常量标记和数据意义即其翻译字面义
     private static final int ID_COLUMN                    = 0;
     private static final int ALERTED_DATE_COLUMN          = 1;
     private static final int BG_COLOR_ID_COLUMN           = 2;
@@ -54,7 +39,7 @@ public class NoteItemData {
     private static final int TYPE_COLUMN                  = 9;
     private static final int WIDGET_ID_COLUMN             = 10;
     private static final int WIDGET_TYPE_COLUMN           = 11;
-
+    //常量标记和数据意义即其翻译字面义
     private long mId;
     private long mAlertDate;
     private int mBgColorId;
@@ -69,14 +54,15 @@ public class NoteItemData {
     private int mWidgetType;
     private String mName;
     private String mPhoneNumber;
-
+ 
     private boolean mIsLastItem;
     private boolean mIsFirstItem;
     private boolean mIsOnlyOneItem;
     private boolean mIsOneNoteFollowingFolder;
     private boolean mIsMultiNotesFollowingFolder;
-
-    public NoteItemData(Context context, Cursor cursor) {
+    //初始化NoteItemData，主要利用光标cursor获取的东西
+    public NoteItemData(Context context, Cursor  cursor) {
+    	//getxxx为转换格式
         mId = cursor.getLong(ID_COLUMN);
         mAlertDate = cursor.getLong(ALERTED_DATE_COLUMN);
         mBgColorId = cursor.getInt(BG_COLOR_ID_COLUMN);
@@ -91,133 +77,138 @@ public class NoteItemData {
         mType = cursor.getInt(TYPE_COLUMN);
         mWidgetId = cursor.getInt(WIDGET_ID_COLUMN);
         mWidgetType = cursor.getInt(WIDGET_TYPE_COLUMN);
-
+ 
+        //初始化号码信息
         mPhoneNumber = "";
         if (mParentId == Notes.ID_CALL_RECORD_FOLDER) {
             mPhoneNumber = DataUtils.getCallNumberByNoteId(context.getContentResolver(), mId);
-            if (!TextUtils.isEmpty(mPhoneNumber)) {
+            if (!TextUtils.isEmpty(mPhoneNumber)) {//mphonenumber里有符合字符串，则用contart功能连接
                 mName = Contact.getContact(context, mPhoneNumber);
                 if (mName == null) {
                     mName = mPhoneNumber;
                 }
             }
         }
-
+ 
         if (mName == null) {
             mName = "";
         }
         checkPostion(cursor);
     }
-
+    ///根据鼠标的位置设置标记，和位置
     private void checkPostion(Cursor cursor) {
+    	//初始化几个标记，cursor具体功能笔记中已提到，不一一叙述
         mIsLastItem = cursor.isLast() ? true : false;
         mIsFirstItem = cursor.isFirst() ? true : false;
         mIsOnlyOneItem = (cursor.getCount() == 1);
+        //初始化“多重子文件”“单一子文件”2个标记
         mIsMultiNotesFollowingFolder = false;
         mIsOneNoteFollowingFolder = false;
-
-        if (mType == Notes.TYPE_NOTE && !mIsFirstItem) {
+ 
+        //主要是设置上诉2标记
+        if (mType == Notes.TYPE_NOTE && !mIsFirstItem) {//若是note格式并且不是第一个元素
             int position = cursor.getPosition();
-            if (cursor.moveToPrevious()) {
+            if (cursor.moveToPrevious()) {//获取光标位置后看上一行
                 if (cursor.getInt(TYPE_COLUMN) == Notes.TYPE_FOLDER
-                        || cursor.getInt(TYPE_COLUMN) == Notes.TYPE_SYSTEM) {
+                        || cursor.getInt(TYPE_COLUMN) == Notes.TYPE_SYSTEM) {//若光标满足系统或note格式
                     if (cursor.getCount() > (position + 1)) {
-                        mIsMultiNotesFollowingFolder = true;
+                        mIsMultiNotesFollowingFolder = true;//若是数据行数大于但前位置+1则设置成正确
                     } else {
-                        mIsOneNoteFollowingFolder = true;
+                        mIsOneNoteFollowingFolder = true;//否则单一文件夹标记为true
                     }
                 }
-                if (!cursor.moveToNext()) {
+                if (!cursor.moveToNext()) {//若不能再往下走则报错
                     throw new IllegalStateException("cursor move to previous but can't move back");
                 }
             }
         }
     }
-
+    //都是获取相关标记
     public boolean isOneFollowingFolder() {
         return mIsOneNoteFollowingFolder;
     }
-
+ 
     public boolean isMultiFollowingFolder() {
         return mIsMultiNotesFollowingFolder;
     }
-
+ 
     public boolean isLast() {
         return mIsLastItem;
     }
-
+ 
     public String getCallName() {
         return mName;
     }
-
+ 
     public boolean isFirst() {
         return mIsFirstItem;
     }
-
+ 
     public boolean isSingle() {
         return mIsOnlyOneItem;
     }
-
+ 
     public long getId() {
         return mId;
     }
-
+ 
     public long getAlertDate() {
         return mAlertDate;
     }
-
+ 
     public long getCreatedDate() {
         return mCreatedDate;
     }
-
+ 
     public boolean hasAttachment() {
         return mHasAttachment;
     }
-
+ 
     public long getModifiedDate() {
         return mModifiedDate;
     }
-
+ 
     public int getBgColorId() {
         return mBgColorId;
     }
-
+ 
     public long getParentId() {
         return mParentId;
     }
-
+ 
     public int getNotesCount() {
         return mNotesCount;
     }
-
+ 
     public long getFolderId () {
         return mParentId;
     }
-
+ 
     public int getType() {
         return mType;
     }
-
+ 
     public int getWidgetType() {
         return mWidgetType;
     }
-
+ 
     public int getWidgetId() {
         return mWidgetId;
     }
-
+ 
     public String getSnippet() {
         return mSnippet;
     }
-
+ 
     public boolean hasAlert() {
         return (mAlertDate > 0);
     }
-
+ 
+    //若数据父id为保存至文件夹模式的id且满足电话号码单元不为空，则isCallRecord为true
     public boolean isCallRecord() {
         return (mParentId == Notes.ID_CALL_RECORD_FOLDER && !TextUtils.isEmpty(mPhoneNumber));
     }
-
+ 
     public static int getNoteType(Cursor cursor) {
         return cursor.getInt(TYPE_COLUMN);
     }
--- app/src/main/java/net/micode/notes/ui/NotesListItem.java
+++ app/src/main/java/net/micode/notes/ui/NotesListItem.java
@@ -1,21 +1,5 @@
-/*
- * Copyright (c) 2010-2011, The MiCode Open Source Community (www.micode.net)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package net.micode.notes.ui;
-
+ 
 import android.content.Context;
 import android.text.format.DateUtils;
 import android.view.View;
@@ -23,44 +7,50 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
+ 
 import net.micode.notes.R;
 import net.micode.notes.data.Notes;
 import net.micode.notes.tool.DataUtils;
 import net.micode.notes.tool.ResourceParser.NoteItemBgResources;
-
-
+ 
+ 
+//创建便签列表项目选项
 public class NotesListItem extends LinearLayout {
-    private ImageView mAlert;
-    private TextView mTitle;
-    private TextView mTime;
-    private TextView mCallName;
-    private NoteItemData mItemData;
-    private CheckBox mCheckBox;
-
+    private ImageView mAlert;   //图片
+    private TextView mTitle;  //标题
+    private TextView mTime;   //时间
+    private TextView mCallName;    //名字
+    private NoteItemData mItemData; //标签数据
+    private CheckBox mCheckBox;    //打钩框
+ 
+    //初始化列表
     public NotesListItem(Context context) {
-        super(context);
-        inflate(context, R.layout.note_item, this);
+        super(context);   //super()调整调用父类构造函数的顺序
+        inflate(context, R.layout.note_item, this);//Inflate可用于将一个xml中定义的布局控件找出来,这里的xml是r。layout
+        //findViewById用于从contentView中查找指定ID的View，转换出来的形式根据需要而定;
         mAlert = (ImageView) findViewById(R.id.iv_alert_icon);
         mTitle = (TextView) findViewById(R.id.tv_title);
         mTime = (TextView) findViewById(R.id.tv_time);
         mCallName = (TextView) findViewById(R.id.tv_name);
         mCheckBox = (CheckBox) findViewById(android.R.id.checkbox);
     }
-
+  ///根据data的属性对各个控件的属性的控制，主要是可见性Visibility，内容setText，格式setTextAppearance
     public void bind(Context context, NoteItemData data, boolean choiceMode, boolean checked) {
         if (choiceMode && data.getType() == Notes.TYPE_NOTE) {
-            mCheckBox.setVisibility(View.VISIBLE);
-            mCheckBox.setChecked(checked);
+            mCheckBox.setVisibility(View.VISIBLE);  ///设置可见行为可见
+            mCheckBox.setChecked(checked);         ///格子打钩
         } else {
             mCheckBox.setVisibility(View.GONE);
         }
-
+ 
         mItemData = data;
+        ///设置控件属性，一共三种情况，由data的id和父id是否与保存到文件夹的id一致来决定
         if (data.getId() == Notes.ID_CALL_RECORD_FOLDER) {
             mCallName.setVisibility(View.GONE);
             mAlert.setVisibility(View.VISIBLE);
+            //设置该textview的style
             mTitle.setTextAppearance(context, R.style.TextAppearancePrimaryItem);
+            //settext为设置内容
             mTitle.setText(context.getString(R.string.call_record_folder_name)
                     + context.getString(R.string.format_folder_files_count, data.getNotesCount()));
             mAlert.setImageResource(R.drawable.call_record);
@@ -69,8 +59,9 @@ public class NotesListItem extends LinearLayout {
             mCallName.setText(data.getCallName());
             mTitle.setTextAppearance(context,R.style.TextAppearanceSecondaryItem);
             mTitle.setText(DataUtils.getFormattedSnippet(data.getSnippet()));
+            ///关于闹钟的设置
             if (data.hasAlert()) {
-                mAlert.setImageResource(R.drawable.clock);
+                mAlert.setImageResource(R.drawable.clock);//图片来源的设置
                 mAlert.setVisibility(View.VISIBLE);
             } else {
                 mAlert.setVisibility(View.GONE);
@@ -78,7 +69,7 @@ public class NotesListItem extends LinearLayout {
         } else {
             mCallName.setVisibility(View.GONE);
             mTitle.setTextAppearance(context, R.style.TextAppearancePrimaryItem);
-
+            ///设置title格式
             if (data.getType() == Notes.TYPE_FOLDER) {
                 mTitle.setText(data.getSnippet()
                         + context.getString(R.string.format_folder_files_count,
@@ -87,35 +78,37 @@ public class NotesListItem extends LinearLayout {
             } else {
                 mTitle.setText(DataUtils.getFormattedSnippet(data.getSnippet()));
                 if (data.hasAlert()) {
-                    mAlert.setImageResource(R.drawable.clock);
+                    mAlert.setImageResource(R.drawable.clock);///设置图片来源
                     mAlert.setVisibility(View.VISIBLE);
                 } else {
                     mAlert.setVisibility(View.GONE);
                 }
             }
         }
-        mTime.setText(DateUtils.getRelativeTimeSpanString(data.getModifiedDate()));
-
+        ///设置内容，获取相关时间，从data里编辑的日期中获取
+        mTime. setText(DateUtils.getRelativeTimeSpanString(data.getModifiedDate()));
+       
         setBackground(data);
     }
-
+    //根据data的文件属性来设置背景
     private void setBackground(NoteItemData data) {
         int id = data.getBgColorId();
-        if (data.getType() == Notes.TYPE_NOTE) {
+        if (data.getType() == Notes.TYPE_NOTE) { //若是note型文件，则分4种情况
+        	//单个数据且只有一个子文件夹
             if (data.isSingle() || data.isOneFollowingFolder()) {
                 setBackgroundResource(NoteItemBgResources.getNoteBgSingleRes(id));
-            } else if (data.isLast()) {
+            } else if (data.isLast()) {//是最后一个数据
                 setBackgroundResource(NoteItemBgResources.getNoteBgLastRes(id));
-            } else if (data.isFirst() || data.isMultiFollowingFolder()) {
+            } else if (data.isFirst() || data.isMultiFollowingFolder()) {//是一个数据并有多个子文件夹
                 setBackgroundResource(NoteItemBgResources.getNoteBgFirstRes(id));
             } else {
                 setBackgroundResource(NoteItemBgResources.getNoteBgNormalRes(id));
             }
         } else {
+        	//若不是note直接调用文件夹的背景来源
             setBackgroundResource(NoteItemBgResources.getFolderBgRes());
         }
     }
-
     public NoteItemData getItemData() {
         return mItemData;
     }
